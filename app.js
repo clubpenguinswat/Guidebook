3d2088325669ad23d2c9182a9ccc80433923b340
SWAT <160239444+clubpenguinswat@users.noreply.github.com>
2024-11-20 15:11:50 +0000

    Infobox, routing, design

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 
 let body = document.body;
-let content = document.querySelector(`main`);
+let content = document.querySelector("main");
+let infobox = document.querySelector("div#infobox");
+let currentTab = "Overview";
 
-function switchTab(tabName) {
+fetch(`./definitions.json`).then(json => {
+  return json.text();
+}).then(raw => {
+  definitions = JSON.parse(raw);
+});
+
+function onLoad() {
+  if (location.hash.replace("#", "") != "") {
+    switchTab(location.hash.replace(`#`, ``));
+  } else {
+    switchTab('Overview');
+  }
+}
+
+async function switchTab(tabName) {
+
   if (!tabName) return new Error("Invalid tab name");
 
   let path = tabName.replace("#", "");
 
-  fetch(`./guides/${path}.html`).then(content => {
+  await fetch(`./guides/${path}.html`).then(content => {
     return content.text();
   }).then(raw => {
     content.innerHTML = raw;
@@ -18,5 +35,20 @@ function switchTab(tabName) {
   } else {
     body.classList.add("centerMode");
   }
+
+  console.log(`aside a[href="#${tabName}"]`);
+  document.querySelector(`aside a[href="#${currentTab}"]`).classList.remove("active");
+  document.querySelector(`aside a[href="#${tabName}"]`).classList.add("active");
+  currentTab = tabName;
+
+}
+
+function define(dfnElement) {
+
+  let definition = definitions[dfnElement.innerText];
+
+  infobox.style.display = "flex";
+  document.querySelector(`div#infobox img`).src = definition.thumbnail;
+  document.querySelector(`div#infobox p`).innerHTML = definition.definition;
 
 }
