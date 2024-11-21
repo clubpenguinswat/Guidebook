fc2b50f19eefc0a7f56579e49599e7a82696fd65
SWAT <160239444+clubpenguinswat@users.noreply.github.com>
2024-11-21 18:57:27 +0000

    Content, infobox, textblocks, definitions, design

--- app.js
+++ app.js
@@ -46,19 +46,61 @@ async function switchTab(tabName) {
     body.classList.add("centerMode");
   }
 
-  console.log(`aside a[href="#${tabName}"]`);
   document.querySelector(`aside a[href="#${currentTab}"]`).classList.remove("active");
   document.querySelector(`aside a[href="#${tabName}"]`).classList.add("active");
   currentTab = tabName;
+
+  let dfnElements = document.querySelectorAll(`dfn`);
+  dfnElements.forEach(function(dfnElement) {
+    dfnElement.setAttribute(`onclick`, `define(this)`);
+  });
+
+  let textblocks = document.querySelectorAll(`div.textblock`);
+  textblocks.forEach(function(textblock) {
+    let copyButton = document.createElement("button");
+    copyButton.innerHTML = "Copy";
+    copyButton.setAttribute("onclick", "copyText(this)");
+    textblock.appendChild(copyButton);
+  });
 
 }
 
 function define(dfnElement) {
 
-  let definition = definitions[dfnElement.innerText];
+  let definition = definitions[dfnElement.innerText.toLowerCase()];
 
-  infobox.style.display = "flex";
-  document.querySelector(`div#infobox img`).src = definition.thumbnail;
+  if (!definition) {
+    definition = {
+      definition: `Error! Could not find a definition for <b>${dfnElement.innerText}</b>.`
+    };
+  }
+
+  infobox.style.bottom = "5%";
+  document.querySelector(`div#infobox img`).src = definition.thumbnail ?? 'logo.png';
   document.querySelector(`div#infobox p`).innerHTML = definition.definition;
 
+  hideInfobox(30000);
+
+}
+
+function hideInfobox(timeout) {
+  function hide() {
+    infobox.style.bottom = "-50%";
+  }
+
+  if (timeout) {
+    setTimeout(() => {
+      hide();
+    }, timeout)
+  } else {
+    hide();
+  }
+}
+
+async function copyText(element) {
+  await navigator.clipboard.writeText(element.parentElement.childNodes[0].data.trim());
+  element.innerHTML = "Copied";
+  setTimeout(() => {
+    element.innerHTML = "Copy";
+  }, 5000);
 }
